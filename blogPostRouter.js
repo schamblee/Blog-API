9f2052ba0d7e592821758a87a91f8e040a434904
stephanie Chamblee <stephaniechamblee@stephanies-MacBook-Air.local>
2018-03-03 15:38:17 -0500

    blog router

---
+++ blogPostRouter.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models');
+
+// convenience function for generating lorem text for blog
+// posts we initially add below
+function lorem() {
+  return 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod ' +
+    'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, '
+    'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ' +
+    'consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ' +
+    'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non ' +
+    'proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+}
+
+// seed some posts so initial GET requests will return something
+BlogPosts.create(
+  '10 things -- you won\'t believe #4', lorem(), 'Billy Bob');
+BlogPosts.create(
+  'Lions and tigers and bears oh my', lorem(), 'Lefty Lil');
+
+
+app.get('/blogposts', (req, res) => {
+  res.json(BlogPosts.get());
+});
+
+app.post('/blogposts', jsonParser, (req, res) => {
+  // ensure `title`, 'content' and `author` are in request body
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+  res.status(201).json(item);
+});
+
+// when PUT request comes in with updated blog, ensure has
+// required fields. also ensure that item id in url path, and
+// item id in updated item object match. if problems with any
+// of that, log error and send back status code 400. otherwise
+// call `BlogPost.update` with updated item.
+app.put('/blogposts/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog \`${req.params.id}\``);
+  BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: author
+  });
+  res.status(204).end();
+});
+
+// when DELETE request comes in with an id in path,
+// try to delete that item from BlogPosts.
+app.delete('/blogposts/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog \`${req.params.ID}\``);
+  res.status(204).end();
+});
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+const blogPostRouter = require('./blogPostRouter');
+
+// log the http layer
+app.use(morgan('common'));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.use('/blogposts', blogPostRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
